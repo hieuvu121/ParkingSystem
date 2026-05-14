db5f447071709dcd02c438f8bfae56b5426b2fdc
hieuvu121 <hieubeobilabi@gmail.com>
2026-05-14 16:54:32 +1000

    feat: add apiFetch wrapper with Bearer token support

---
+++ ParkingSystemFrontend/src/api/client.js
@@ -0,0 +1,11 @@
+export function apiFetch(path, options = {}) {
+  const token = localStorage.getItem('token');
+  return fetch(path, {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      ...options.headers,
+    },
+  });
+}
